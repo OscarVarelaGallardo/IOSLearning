87c597012859656acc4c81f7466afa830f66a396
Oscar <64058694+OscarVarelaDev@users.noreply.github.com>
2023-08-14 11:59:18 -0600

    Initial Commit

---
+++ Modal/ModalApp.swift
@@ -0,0 +1,20 @@
+//
+//  ModalApp.swift
+//  Modal
+//
+//  Created by Oscar Varela on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ModalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
